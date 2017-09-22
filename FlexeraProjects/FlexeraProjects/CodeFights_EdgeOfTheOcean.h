d47dff80e4aca420960c46cab5aeb8de0b196d57
Hassan-iqbal <Hassan_iqbal2812@outlook.com>
2017-09-22 09:13:11 +0100

    New Header file
    Made new header file for the next solution.

---
+++ FlexeraProjects/FlexeraProjects/CodeFights_EdgeOfTheOcean.h
@@ -0,0 +1,16 @@
+//
+//  CodeFights_EdgeOfTheOcean.h
+//  FlexeraProjects
+//
+//  Created by Hassan Iqbal on 22/09/2017.
+//  Copyright © 2017 Hassan Iqbal. All rights reserved.
+//
+
+#ifndef CodeFights_EdgeOfTheOcean_h
+#define CodeFights_EdgeOfTheOcean_h
+
+
+#endif /* CodeFights_EdgeOfTheOcean_h */
+
+
+
